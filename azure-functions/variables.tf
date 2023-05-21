40cce87774ecd01fc3bc54d485846770c76a2252
MarcienD <114868544+MarcienD@users.noreply.github.com>
2023-05-21 21:33:44 +0200

    Create variables.tf

---
+++ azure-functions/variables.tf
@@ -0,0 +1,28 @@
+variable "resource_group_name" {
+  type = string
+  default = "example-resource-group"
+}
+
+variable "location" {
+  type = string
+  default = "westeurope"
+}
+
+variable "storage_account_name" {
+  type = string
+  default = "example-storage"
+}
+variable "app_service_plan_name" {
+  type = string
+  default = "example-app-service-plan"
+}
+
+variable "function_app_name" {
+  type = string
+  default = "example-function-app"
+}
+
+variable "function_name" {
+  type = string
+  default = "example-function"
+}
