03c07ea91de015b9938dced480f5354f146d7f55
MarcienD <114868544+MarcienD@users.noreply.github.com>
2023-05-21 21:33:14 +0200

    main.tf ze zmiennymi

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,1 +1,60 @@
+resource "azurerm_resource_group" "example" {
+   name   = var.resource_group_name
+   location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = var.app_service_plan_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                      = var.function_app_name
+  resource_group_name       = azurerm_resource_group.example.name
+  location                  = azurerm_resource_group.example.location
+  service_plan_id           = azurerm_app_service_plan.example.id
+  storage_account_name      = azurerm_storage_account.example.name
+  site_config {
+  }
+}
+
+resource "azurerm_function_app_function" "example" {
+  name                      = var.function_name
+  function_app_id           = azurerm_linux_function_app.example.id
+    config_json = <<JSON
+    {
+      "bindings": [
+        {
+          "authLevel": "anonymous",
+          "type": "httpTrigger",
+          "direction": "in",
+          "name": "req",
+          "methods": [
+            "get",
+            "post"
+          ]
+        },
+        {
+          "type": "http",
+          "direction": "out",
+          "name": "$return"
+        }
+      ]
+    }
+  JSON
+  
+}
 
