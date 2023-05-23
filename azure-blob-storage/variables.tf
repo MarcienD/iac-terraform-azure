2c1ab08a276cb47f15eae7f7e2d95bde576baa39
MarcienD <114868544+MarcienD@users.noreply.github.com>
2023-05-23 17:41:00 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,24 @@
+variable "resource_group_name" {
+  type    = string
+  default = "example-resource-group"
+}
 
+variable "resource_group_location" {
+  type    = string
+  default = "west europe"
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = "example-storage-account"
+}
+
+variable "storage_container_name" {
+  type    = string
+  default = "example-container"
+}
+
+variable "storage_blob_name" {
+  type    = string
+  default = "example-blob"
+}
