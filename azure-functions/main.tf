60d7445db317eac8e9f258a811648bf0b36a2753
MarcienD <114868544+MarcienD@users.noreply.github.com>
2023-05-23 17:50:04 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_storage_account" "example" {
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = var.storage_account_tier
-  account_replication_type = var.account_replication_type
+  account_replication_type = var.storage_account_replication_type
 }
 
 resource "azurerm_app_service_plan" "example" {
