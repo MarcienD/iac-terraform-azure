75d4d2a60a63023754e87a83c54e52a95d487fa6
MarcienD <114868544+MarcienD@users.noreply.github.com>
2023-05-23 17:02:26 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,1 +1,15 @@
+resource "azurerm_resource_group" "example" {
+   name   = var.resource_group_name
+   location = var.resoutce_group_location
+}
 
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurem_storage_container" "example" {
+  
